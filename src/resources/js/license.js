cde6769defe260fd0138f244b341dc9d380ab19f
maxthestranger <thestrangermax@gmail.com>
2021-02-15 01:43:29 +0300

    changes to the library

---
+++ src/resources/js/license.js
@@ -0,0 +1,14 @@
+/*
+
+Theme: MTS
+Version: 1.1.0
+Product Page: https://maxthestranger.com/themes/mts
+License: MIT
+Author: max the stranger
+Author URI: https://maxthestranger.com
+
+---
+
+Copyright 2021 MTS
+
+*/
